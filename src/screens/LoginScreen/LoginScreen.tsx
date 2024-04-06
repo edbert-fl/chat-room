49d45b0082a31f1d13958f58c92d76da424121c1
Edbert Felix Lim <edbert.fl@gmail.com>
2024-04-06 21:51:39 +1100

    Fixed syntax issue with error handling

--- src/screens/LoginScreen/LoginScreen.tsx
+++ src/screens/LoginScreen/LoginScreen.tsx
@@ -1,13 +1,13 @@
-import React, { useContext, useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { UserUpdateContext } from  "../../context/UserContextProvider.tsx";
-import './LoginScreen.css'
-import axios from 'axios';
+import React, { useContext, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { UserUpdateContext } from "../../context/UserContextProvider.tsx";
+import "./LoginScreen.css";
+import axios from "axios";
 
 export const LoginScreen = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
   const navigation = useNavigate();
 
@@ -15,32 +15,40 @@ export const LoginScreen = () => {
 
   const handleLogin = async () => {
     // Add your login logic here
-    console.log('Username:', username);
-    console.log('Password:', password);
-    console.log("Server_url:", process.env.REACT_APP_HEROKU_URL)
+    console.log("Username:", username);
+    console.log("Password:", password);
+    console.log("Server_url:", process.env.REACT_APP_HEROKU_URL);
     try {
-      const response = await axios.post(`${process.env.REACT_APP_HEROKU_URL}/login/user`, { username, password });
-      console.log(response)
+      const response = await axios.post(
+        `${process.env.REACT_APP_HEROKU_URL}/login/user`,
+        {
+          username: username,
+          password: password,
+        }
+      );
+      console.log(response);
     } catch (error) {
-      console.log(error)
       if (error.response && error.response.status === 401) {
-        setError('Incorrect password.');
+        setError("Incorrect password.");
         return;
-    } else if (error.response && error.response.status === 404) {
-        setError('User not found.');
+      } else if (error.response && error.response.status === 404) {
+        setError("User not found.");
         return;
-    } else {
-        setError('An error occurred.');
+      } else if (error.response && error.response.status === 400) {
+        setError("Please input username and password.");
         return;
+      } else {
+        setError("An error occurred.");
+        return;
+      }
     }
-    }
-    
-    setCurrUser({
-      id: 1, 
-      name: username
-    })
 
-    navigation("/")
+    setCurrUser({
+      id: 1,
+      name: username,
+    });
+
+    navigation("/");
   };
 
   return (
@@ -62,14 +70,21 @@ export const LoginScreen = () => {
           className="w-full px-3 py-2 mb-4 placeholder-gray-500 text-gray-700 rounded-md bg-gray-100 focus:outline focus:outline-teal-300"
         />
       </div>
-      <button onClick={handleLogin} className="w-full bg-teal-500 hover:bg-teal-600 text-white font-bold py-2 px-4 rounded-md focus:outline-none focus:ring focus:ring-teal-300">Login</button>
-      <div className='text-center'>
+      <button
+        onClick={handleLogin}
+        className="w-full bg-teal-500 hover:bg-teal-600 text-white font-bold py-2 px-4 rounded-md focus:outline-none focus:ring focus:ring-teal-300"
+      >
+        Login
+      </button>
+      <div className="text-center">
         <p>{error}</p>
         <p>Don't have an account?</p>
-        <p className="text-teal-600"><Link to="/register">Register here!</Link></p>
+        <p className="text-teal-600">
+          <Link to="/register">Register here!</Link>
+        </p>
       </div>
     </div>
   );
-}
+};
 
 export default LoginScreen;
